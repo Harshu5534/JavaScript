5cd3ed70b2b8813a2b758110e0a55f543e5212bf
Harshal Arun Patil <harshalapatil2796@gmail.com>
2022-06-15 15:15:39 +0530

    [Harshal]Add:Calculate Employee Wage For Month Assuming Days 20

--- EmployeeWageProblem.js
+++ EmployeeWageProblem.js
@@ -15,6 +15,7 @@ const IS_FULL_TIME=2;
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOUR=8;
 const WAGE_PER_HOUR=20;
+const NUM_OF_WORKING_DAYS=20;
 {
     function getWorkingHours(empCheck)
     {
@@ -28,10 +29,12 @@ const WAGE_PER_HOUR=20;
                 return 0;
         }
     }
-    let empCheck=Math.floor(Math.random() * 10) % 3;
-    let empHrs=getWorkingHours(empCheck);
-    let empWage=empHrs*WAGE_PER_HOUR;
-    console.log("Hours: " +empHrs+ "\nEmployee Wage: " + empWage);
+    let totalEmpHrs=0;
+    for(let day=0; day<NUM_OF_WORKING_DAYS; day++)
+    {
+        let empCheck=Math.floor(Math.random() * 10) % 3;
+        totalEmpHrs += getWorkingHours(empCheck);
+    }
+    let empWage=totalEmpHrs * WAGE_PER_HOUR;
+    console.log("Total Hours: " + totalEmpHrs +"\nEmployee Wage: " + empWage);
 }
-
-
